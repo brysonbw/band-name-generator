2204c94d7c13d803b6622164198ee616c8d69907
“bryson” <brysonward77@gmail.com>
2021-09-14 03:27:20 -0400

    Band Name Generator Program

---
+++ script.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Name Generator.')
+print('What\'s the name of the city you grew up in?')
+userCityName = input()
+print('What\'s your pet\'s name?')
+userPetName = input()
+userBandName = print(f'Your band name could be {userCityName} {userPetName}')
